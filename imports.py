f89bbc84fa84924b1553c7cb3da6050ff671cb74
Simone Malesardi <simomale98@gmail.com>
2023-08-18 11:59:02 +0200

    train & test: feature computing and graphframes

--- imports.py
+++ imports.py
@@ -1,17 +1,28 @@
-import pandas as pd
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, expr, count, to_timestamp, monotonically_increasing_id, desc, when, sum as _sum, monotonically_increasing_id, min, max, datediff
-
-from pyspark.sql.functions import dayofmonth, weekofyear, month, year
-from pyspark.sql.window import Window
-from pyspark.sql.types import *
-from pyspark.ml.feature import StandardScaler, VectorAssembler
-
 import os 
-
 import numpy as np
+import pandas as pd
+import seaborn as sns 
+from math import isnan
+import multiprocessing
 
 import matplotlib.pyplot as plt
-from math import isnan
+from FeatureManager import *
+from MyGraph import *
 
-import multiprocessing
+from graphframes import *
+
+########## START - PYSPARK ##########
+from pyspark import SparkContext, SparkConf
+
+from pyspark.sql import SparkSession, SQLContext, Row
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+from pyspark.sql.functions import col, expr, count, to_timestamp, monotonically_increasing_id, \
+    desc, sum as _sum, min, max, rand, when, \
+    datediff, dayofmonth, weekofyear, month, year, hour, dayofweek, \
+    unix_timestamp, array, lit
+
+from pyspark.ml import Pipeline
+from pyspark.ml.stat import Correlation
+from pyspark.ml.feature import StandardScaler, VectorAssembler, StringIndexer 
+########## END - PYSPARK ##########
